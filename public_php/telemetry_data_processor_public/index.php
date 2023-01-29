c30fc89012e3cf792298892ccf449911f68d8c3b
Billy Parker <billyeparker@gmail.com>
2023-01-29 03:30:08 +0000

    commit ctec3110

---
+++ public_php/telemetry_data_processor_public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+/** all applications will call a bootstrap.php script,
+which will contain configuration and routing instructions
+This is the single point of entry to your application
+ */
+
+include 'telemetry_data_processor_application/bootstrap.php';
